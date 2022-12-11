6567cb1f588e62d2faebacca6f00450a8d2f8e6f
eliszewski <liszewski31098@gmail.com>
2022-12-11 18:21:38 -0500

    "add"

--- index.ts
+++ index.ts
@@ -1,16 +1,55 @@
-// Import stylesheets
-import './style.css';
-
-
 const form: HTMLFormElement | null = document.querySelector('#defineform');
+
 if(form != null) {
   form.onsubmit = () => {
-  const formData = new FormData(form);
+    const formData = new FormData(form);
 
-  console.log(formData);
-  const text = formData.get('defineword') as string;
-  console.log(text);
-  return false; // prevent reload
-}
-}
-;
+    console.log(formData);
+    const text = formData.get('defineword') as string;
+    const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/" + text;
+    fetch(apiUrl)
+    .then((response) => { 
+      return response.json()
+    })
+    .then((data) => {
+      const oldList = document.querySelector('.list-group');
+
+      const listParent: HTMLUListElement = document.createElement('ul');
+      listParent.classList.add('list-group');
+
+      const firstList: HTMLLIElement = document.createElement('li');
+      firstList.classList.add('list-group-item');
+      firstList.textContent = data[0].word;
+      listParent.append(firstList);
+
+      for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].meanings.length; j++) {
+            const partOfSpeechItem: HTMLLIElement = document.createElement('li');
+            partOfSpeechItem.textContent = 'Parts of Speech: ' + data[i].meanings[j].partOfSpeech;
+
+            const definitionTitle: HTMLLIElement = document.createElement('li');
+            definitionTitle.textContent = 'Definition:';
+
+            const definitionItemList: HTMLUListElement = document.createElement('ul');
+            definitionTitle.append(definitionItemList);
+
+            for (let k = 0; k < data[i].meanings[j].definitions.length; k++) {
+                const definitionItem: HTMLLIElement = document.createElement('li');
+                definitionItem.textContent = data[i].meanings[j].definitions[k].definition;
+                definitionItemList.append(definitionItem);
+            }
+            listParent.append(partOfSpeechItem, definitionTitle);
+        }
+    }
+
+      oldList?.replaceWith(listParent);
+      console.log(data);
+    })
+    .catch((error) => console.log(error));
+
+    console.log(text);
+    return false; // prevent reload
+  };
+};
+
+  
