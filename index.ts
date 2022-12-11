833b94dc3c1709f2285a6f965efdf7ed0de25edd
eliszewski <liszewski31098@gmail.com>
2022-12-11 17:53:32 -0500

    trying things

--- index.ts
+++ index.ts
@@ -2,14 +2,15 @@
 import './style.css';
 
 
-const form: HTMLFormElement = document.querySelector('#defineform');
-
-
-form.onsubmit = () => {
+const form: HTMLFormElement | null = document.querySelector('#defineform');
+if(form != null) {
+  form.onsubmit = () => {
   const formData = new FormData(form);
 
   console.log(formData);
   const text = formData.get('defineword') as string;
   console.log(text);
   return false; // prevent reload
-};
+}
+}
+;
